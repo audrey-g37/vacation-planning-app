63438dc1bbb34f11ddcf18ce736a80311f99813c
Eddy Calderon <edgarcalderon@Edgars-MacBook-Pro.local>
2021-10-03 12:56:44 -0600

    adding reslovers and typeDefs

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,1 +1,56 @@
-const { gql } = require("apollo-server-express");
+const { Budget, Task, Trip, User} = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+
+
+const resolvers = {
+
+    Query: {
+        user: async (parent, args, context) => {
+          if (context.user) {
+            return User.findOne({ _id: context.user._id });
+          }
+          throw new AuthenticationError("You need to be logged in!");
+        },
+      },
+      Mutation: {
+        addUser: async (parent, {username, password }) => {
+          const User = await User.create({ username,password });
+          const token = signToken(User);
+          return { token, User };
+        },
+
+        login: async (parent, { username, password }) => {
+          const User = await User.findOne({ username});
+          if (!User) {
+            throw new AuthenticationError("No profile with this email found!");
+          }
+          const correctPw = await User.isCorrectPassword(password);
+          if (!correctPw) {
+            throw new AuthenticationError("Incorrect password!");
+          }
+          const token = signToken(User);
+          return { token, User };
+        }, 
+
+        addTrip: async (parent, { userId, trip, title, location, startDate, endDate, description }, context) => {
+            // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+            const newTrip = await Trip.create({userId, title, location, startDate, endDate, description})
+            if (context.user) {
+              return User.findOneAndUpdate(
+                { _id: userId },
+                {
+                  $addToSet: { trip: trip  },
+                },
+                {
+                  new: true,
+                  runValidators: true,
+                }
+              );
+            }
+            // If user attempts to execute this mutation and isn’t logged in, throw an error
+            throw new AuthenticationError("You need to be logged in!");
+        },
+}
+}
---
+++ server/schema/typeDefs.js
@@ -0,0 +1,75 @@
+const {gql} = require ("apollo-server-express");
+
+
+
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    password: String!
+    trip:[Trip]
+}
+
+type Trip {
+    _id: ID!
+    title: String!
+    location: String!
+    startDate: Date!
+    endDate: Date!
+    description: String
+}
+
+type Task{
+_id:ID!
+title: String!
+details: String
+dueDate: Date!
+status: Boolean!
+assignee: String!
+}
+
+type Budget {
+    _id:ID!
+    name: String!
+    value: Number!
+    purchaseDate: Date!
+    purchasedBy: String!
+    trip: [Trip]!
+}
+
+type Auth {
+    token: String!
+    user: User
+}
+
+type Query {
+    user:[User]!
+    trip:[Trip]
+    task:[Task]
+    budget: [Budget]
+}
+
+type Mutation {
+
+    addUser(username: String!, password: String!): Auth
+    login(username: String!, password: String!): Auth
+
+    addTrip(title: String!, description: String, location: String!, startDate: Date!, endDate: Date!, userId: ID!): Trip
+    addTask(title: String!, details: String, dueDate: Date!, status: Boolean!, assignee: String!, tripId: ID!): Task
+    addBudget(title: String!, value: Number!, purchaseDate: Date!, purchasedBy: Date!, tripId: ID!): Budget
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+`
+
+module.exports = typeDefs;
