67715027505c34154a7fbe4956c02096ea8c9dd1
Audrey <audrey.gillies@gmail.com>
2021-10-08 23:46:45 -0600

    queries built

--- client/src/pages/ViewTrip(s)/ViewSingleTrip.js
+++ client/src/pages/ViewTrip(s)/ViewSingleTrip.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./ViewSingleTrip.css";
+import {useQuery} from "@apollo/client"
 
 const ViewTrip = () => {
+
   return <h1>This is the view SINGLE trip page</h1>;
 };
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,36 +1,42 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      trip {
-        _id
-      }
-    }
+query ($username: String!) {
+  user (username: $username) {
+	_id
+    username
+    password
   }
+}
 `;
 
 export const QUERY_USERS = gql`
-  query users {
+query {
+  users {
     _id
     username
     password
   }
+}
 `;
 
 export const QUERY_TRIP = gql`
-  query getTrip($tripId: ID!) {
-    trip(tripId: $tripId) {
+query ($tripId: ID!) {
+  trip (tripId: $tripId) {
+    _id
+    title
+    location
+    startDate
+    endDate
+    description
+    tasks {
       _id
-      title
-      location
-      startDate
-      endDate
-      description
+    }
+    budget {
+    _id
     }
   }
+}
 `;
 
 export const QUERY_TRIPS = gql`
@@ -47,48 +53,50 @@ query {
 `;
 
 export const QUERY_TASK = gql`
-  query getTask($taskId: ID!) {
-    task(taskId: $taskId) {
-      _id
-      title
-      details
-      dueDate
-      status
-      assignee
-    }
+query task ($taskId: ID!){
+  task (taskId:$taskId) {
+    _id
+    title
+    details
+    dueDate
+    status
+    assignee
   }
+}
 `;
+
 export const QUERY_TASKS = gql`
-  query getTasks {
-    tasks {
-      _id
-      title
-      details
-      dueDate
-      status
-      assignee
-    }
+query {
+  tasks {
+    _id
+    title
+    details
+    dueDate
+    status
+    assignee
   }
+}
 `;
+
 export const QUERY_BUDGETS = gql`
-  query getBudgets {
-    budgets {
-      _id
-      title
-      value
-      purchaseDate
-      purchasedBy
-    }
+query {
+  budgets {
+    _id
+    title
+ 		value
+    purchaseDate
+    purchasedBy
   }
+}
 `;
 export const QUERY_BUDGET = gql`
-  query getBudget($budgetId: ID!) {
-    budget(budgetId: $budgetId) {
-      _id
-      title
-      value
-      purchaseDate
-      purchasedBy
-    }
+query ($budgetId: ID!) {
+  budget(budgetId: $budgetId) {
+  	_id
+    title
+ 		value
+    purchaseDate
+    purchasedBy
   }
+}
 `;
