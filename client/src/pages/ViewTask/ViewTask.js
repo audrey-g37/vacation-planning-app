8ef0445086918ca754aaf2006f5bb0b6659730d7
Gina Im <eaim928@gmail.com>
2021-10-08 18:51:26 -0600

    testing queries

--- client/src/pages/ViewTask/ViewTask.js
+++ client/src/pages/ViewTask/ViewTask.js
@@ -5,17 +5,20 @@ import { QUERY_TASKS } from "../../utils/queries";
 // import DeleteIcon from "@mui/icons-material/Delete";
 // import IconButton from "@mui/material/IconButton";
 import { DataGrid } from "@mui/x-data-grid";
+import { useParams } from "react-router-dom";
 
-const ViewTasks = () => {
+const ViewTask = () => {
+  const { tripId } = useParams();
   const { loading, data } = useQuery(QUERY_TASKS);
   const tasks = data?.tasks || [];
+  console.log(tasks);
 
   const columns = [
-    { field: "title", headerName: "Title", width: 70 },
-    { field: "details", headerName: "Details", width: 70 },
-    { field: "dueDate", headerName: "Due Date", type: "Date", width: 70 },
-    { field: "status", headerName: "Status", width: 70 },
-    { field: "assignee", headerName: "Assignee", width: 70 },
+    { field: "title", headerName: "Title", width: 150 },
+    { field: "details", headerName: "Details", width: 150 },
+    { field: "dueDate", headerName: "Due Date", type: "Date", width: 150 },
+    { field: "status", headerName: "Status", width: 150 },
+    { field: "assignee", headerName: "Assignee", width: 150 },
   ];
 
   const rows = tasks.map((task) => ({
@@ -25,6 +28,8 @@ const ViewTasks = () => {
     status: task.status,
     assignee: task.assignee,
   }));
+
+  console.log(rows);
 
   return (
     <main>
@@ -39,4 +44,4 @@ const ViewTasks = () => {
   );
 };
 
-export default ViewTasks;
+export default ViewTask;
