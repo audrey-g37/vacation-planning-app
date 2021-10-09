d47451ed225be5e369349cb4b1a9acb10d26c847
Audrey <audrey.gillies@gmail.com>
2021-10-09 11:58:45 -0600

    trying to sort dates

--- client/src/pages/ViewTrip(s)/ViewAllTrips.js
+++ client/src/pages/ViewTrip(s)/ViewAllTrips.js
@@ -2,21 +2,36 @@ import React from "react";
 import "./ViewAllTrips.css";
 import { Container, Row, Col} from "react-bootstrap"
 
+import moment from "moment";
+
 import { useQuery } from '@apollo/client';
 import { QUERY_TRIPS} from '../../utils/queries';
 
 const ViewAllTrips = () => {
+
+moment().format();
 const {loading, data} = useQuery(QUERY_TRIPS);
-const trips = data?.trips || [];
-console.log (trips);
+let allTrips = data?.trips || [];
+
+allTrips = allTrips.map((trip)=> {
+
+})
+
+// allTrips.sort(function ())
+console.log (allTrips);
 
 
   return (
-  <>
  <Container fluid className= "all-trips">
   <Row>
     <h1>All Stored Trips</h1>
     <ul className= "list-of-trips">
+      {/* {allTrips.length>0? allTrips.map((trip) => {
+
+      })
+
+
+      } */}
     <li className= "trip-title">
       <a>Trip 1</a>
       </li>
@@ -24,7 +39,7 @@ console.log (trips);
     </ul>
   </Row>
 </Container>
-    </>)
+)
 };
 
 export default ViewAllTrips;
