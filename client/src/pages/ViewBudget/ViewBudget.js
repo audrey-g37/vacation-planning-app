2b9f8aa500e889e6ff341a1577c138b488b700ba
Adrian <adrian.auch11@gmail.com>
2021-10-09 13:49:30 -0600

    updated budget page

--- client/src/pages/ViewBudget/ViewBudget.js
+++ client/src/pages/ViewBudget/ViewBudget.js
@@ -4,6 +4,15 @@ import { useQuery } from "@apollo/client";
 import { QUERY_BUDGET, QUERY_BUDGETS } from "../../utils/queries";
 import Table from 'react-bootstrap/Table'
 import { REMOVE_BUDGET, UPDATE_BUDGET, ADD_BUDGET } from "../../utils/mutations";
+import { BsTrashFill } from "react-icons/bs";
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import InputGroup from 'react-bootstrap/InputGroup'
+
 
 
 
@@ -17,6 +26,10 @@ const ViewBudget = () => {
 
   return (
     <main>
+      <Container>
+      <Row xs={1} md={2}>
+        <Col>
+        <h2>Trip Expenses</h2>
       <Table striped bordered hover>
   <thead>
     <tr>
@@ -31,11 +44,15 @@ const ViewBudget = () => {
     allExpenses.map((budget) => (
       <tr>
       <td>{budget.title}</td>
-      <td>{budget.value}</td>
+      <td>${budget.value}</td>
       <td>{budget.purchaseDate}</td>
       <td>{budget.purchasedBy}</td>
       <td>
         {" "}
+        <Button variant="outline-danger" class="tashbtn">
+          <BsTrashFill/>
+          {/* logic for delete */}
+        </Button>
       </td>
       </tr>
        ))
@@ -51,6 +68,41 @@ const ViewBudget = () => {
     
   </tbody>
 </Table>
+<p>Trip Total:</p>
+</Col>
+<Col>
+<>
+ <h2>Add A New Expense</h2>
+ <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Expense</Form.Label>
+    <Form.Control type="email" placeholder="Expense Name" />
+  </Form.Group>
+  <Form.Group>
+  <Form.Label htmlFor="inlineFormInputGroupUsername" visuallyHidden>
+        Amount
+      </Form.Label>
+      <InputGroup>
+        <InputGroup.Text>$</InputGroup.Text>
+        <FormControl id="inlineFormInputGroupUsername" placeholder="Amount" />
+      </InputGroup>
+      </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Purchased Date</Form.Label>
+    <Form.Control type="date" placeholder="MM/DD/YYYY" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Purchased By</Form.Label>
+    <Form.Control type="input" placeholder="Name" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</>
+</Col>
+</Row>
+</Container>
 
 
 
@@ -59,3 +111,4 @@ const ViewBudget = () => {
 };
 
 export default ViewBudget;
+
