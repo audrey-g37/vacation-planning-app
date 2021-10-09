bce7eb1e7e99a49f5c70e06f3c967263c5e890f6
Adrian <adrian.auch11@gmail.com>
2021-10-09 12:02:36 -0600

    added a libary

--- client/src/pages/ViewBudget/ViewBudget.js
+++ client/src/pages/ViewBudget/ViewBudget.js
@@ -1,8 +1,61 @@
 import React from "react";
 import "./ViewBudget.css";
+import { useQuery } from "@apollo/client";
+import { QUERY_BUDGET, QUERY_BUDGETS } from "../../utils/queries";
+import Table from 'react-bootstrap/Table'
+import { REMOVE_BUDGET, UPDATE_BUDGET, ADD_BUDGET } from "../../utils/mutations";
+
+
 
 const ViewBudget = () => {
-  return <h1>This is the Budget Page</h1>;
+  // JS 
+  const {loading, data} = useQuery(QUERY_BUDGETS);
+  const allExpenses = data?.budgets || []
+  console.log(allExpenses)
+
+
+
+  return (
+    <main>
+      <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Expense Name</th>
+      <th>Amount</th>
+      <th>Purchased Date</th>
+      <th>Purchased By</th>
+    </tr>
+  </thead>
+  <tbody>
+    {allExpenses.length > 0 ? (
+    allExpenses.map((budget) => (
+      <tr>
+      <td>{budget.title}</td>
+      <td>{budget.value}</td>
+      <td>{budget.purchaseDate}</td>
+      <td>{budget.purchasedBy}</td>
+      <td>
+        {" "}
+      </td>
+      </tr>
+       ))
+       ) : (
+       <tr>
+       <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      </tr>
+          )}
+    
+  </tbody>
+</Table>
+
+
+
+    </main>
+  )
 };
 
 export default ViewBudget;
