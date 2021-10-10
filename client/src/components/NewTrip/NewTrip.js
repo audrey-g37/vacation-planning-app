30a37371dd625ac3c873019f96fb718afb3ca507
Audrey <audrey.gillies@gmail.com>
2021-10-09 21:56:58 -0600

    new trip is storing

--- client/src/components/NewTrip/NewTrip.js
+++ client/src/components/NewTrip/NewTrip.js
@@ -1,8 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewTrip.css";
+import { useQuery, useMutation } from "@apollo/client";
+import { Form, Button } from "react-bootstrap";
+import { ADD_TRIP } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+import { QUERY_USER } from "../../utils/queries";
 
 const NewTrip = () => {
-  return <h1>This is a NewTrip</h1>;
+  const currentUser = Auth.getUsername();
+
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: currentUser },
+  });
+  const userData = data?.user || [];
+  console.log(userData);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const [addTrip, { error }] = useMutation(ADD_TRIP);
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    if (name === "title") {
+      setTitle(value);
+    } else if (name === "description") {
+      setDescription(value);
+    } else if (name === "location") {
+      setLocation(value);
+    } else if (name === "startDate") {
+      setStartDate(value);
+    } else if (name === "endDate") {
+      setEndDate(value);
+    }
+    console.log(title);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    addTrip({
+      variables: {
+        userId: userData._id,
+        title: title,
+        description: description,
+        location: location,
+        startDate: startDate,
+        endDate: endDate,
+      },
+    }).then((data) => {
+      console.log(data);
+      setTitle("");
+      setLocation("");
+      setStartDate("");
+      setEndDate("");
+      setDescription("");
+    });
+  };
+  return (
+    <Form className="Form">
+      <h2>New Trip Details</h2>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Title*</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Location*</Form.Label>
+        <Form.Control
+          type="text"
+          name="location"
+          value={location}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Start Date*</Form.Label>
+        <Form.Control
+          type="date"
+          name="startDate"
+          value={startDate}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>End Date*</Form.Label>
+        <Form.Control
+          type="date"
+          name="endDate"
+          value={endDate}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label> Trip Description</Form.Label>
+        <Form.Control
+          type="text"
+          name="description"
+          value={description}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      {Auth.loggedIn() && (
+        <Button
+          className="add-trip-button"
+          variant="dark"
+          onClick={handleFormSubmit}
+          type="submit"
+        >
+          Add New Trip
+        </Button>
+      )}
+    </Form>
+  );
 };
 
 export default NewTrip;
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,78 +1,22 @@
 import React, { useState } from "react";
 import { useParams, Redirect } from "react-router-dom";
-import { useMutation } from "@apollo/client";
 import { useQuery } from "@apollo/client";
 import Auth from "../../utils/auth";
 import "./Dashboard.css";
-import { Table, Form, Button } from "react-bootstrap";
-import { QUERY_TRIPS, QUERY_USER } from "../../utils/queries";
-import { ADD_TRIP } from "../../utils/mutations";
+import { Table, Button } from "react-bootstrap";
+import { QUERY_TRIP, QUERY_TRIPS } from "../../utils/queries";
+import NewTrip from "../../components/NewTrip/NewTrip";
 
 const Dashboard = () => {
   const { loading, data } = useQuery(QUERY_TRIPS);
   const allTrips = data?.trips || [];
+  const currentUser = Auth.getUsername();
 
-  // const currentUserId = Auth.getUsername();
-  // const { data, loading } = useQuery(QUERY_USER, {
-  //   variables: { username: currentUserId },
-  // });
-  // const userData = data?.user || [];
-
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [location, setLocation] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-
-  const [addTrip, { error }] = useMutation(ADD_TRIP);
-
-  const handleInputChange = (event) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-
-    if (name === "title") {
-      setTitle(value);
-    } else if (name === "description") {
-      setDescription(value);
-    } else if (name === "location") {
-      setLocation(value);
-    } else if (name === "startDate") {
-      setStartDate(value);
-    } else if (name === "endDate") {
-      setEndDate(value);
-    }
-    console.log(title);
-    console.log(location);
-    console.log(startDate);
-    console.log(endDate);
-    console.log(description);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    // addTrip({
-    //   variables: {
-    //     userId: userData._id,
-    //     title: title,
-    //     description: description,
-    //     location: location,
-    //     startDate: startDate,
-    //     endDate: endDate,
-    //   },
-    // }).then((data) => {
-    //   console.log(data);
-
-    setTitle("");
-    setLocation("");
-    setStartDate("");
-    setEndDate("");
-    setDescription("");
-    // });
-  };
+  console.log(allTrips);
 
   return (
     <div className="Home">
-      <h2 className="Title">Welcome, Grip Member!</h2>
+      <h2 className="Title">Welcome, {currentUser}</h2>
       <section className="Dashboard">
         <Table className="Table" responsive>
           <thead>
@@ -112,66 +56,9 @@ const Dashboard = () => {
             </Button>{" "}
           </tbody>
         </Table>
-
-        <Form className="Form">
-          <h2>New Trip Details</h2>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Title*</Form.Label>
-            <Form.Control
-              type="text"
-              name="title"
-              value={title}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Location*</Form.Label>
-            <Form.Control
-              type="text"
-              name="location"
-              value={location}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Start Date*</Form.Label>
-            <Form.Control
-              type="date"
-              name="startDate"
-              value={startDate}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>End Date*</Form.Label>
-            <Form.Control
-              type="date"
-              name="endDate"
-              value={endDate}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label> Trip Description</Form.Label>
-            <Form.Control
-              type="text"
-              name="description"
-              value={description}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-          {Auth.loggedIn() && (
-            <Button
-              className="add-trip-button"
-              variant="dark"
-              onClick={handleFormSubmit}
-              type="submit"
-            >
-              Add New Trip
-            </Button>
-          )}
-        </Form>
       </section>
+
+      <NewTrip />
     </div>
   );
 };
