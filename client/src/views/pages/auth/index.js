70d4890c1a72fa272f29d022e9de0d4ab30f5f2d
Audrey Gillies <audrey.gillies@gmail.com>
2023-04-25 13:23:16 -0600

    updating the last date modified

--- client/src/views/pages/auth/index.js
+++ client/src/views/pages/auth/index.js
@@ -1,6 +1,3 @@
-// mui imports
-import { Grid, useTheme } from '@mui/material';
-
 // project imports
 import Header from 'views/components/specific/Header';
 import Footer from 'views/components/specific/Footer';
@@ -9,10 +6,9 @@ import AuthLayout from 'views/pages/auth/AuthLayout';
 import { UnderConstruction } from 'views/pages';
 
 const AuthMain = () => {
-	const theme = useTheme();
 	// *using to disable app/features while I refactor and enhance app
 	const underConstruction = process.env.NODE_ENV === 'production';
-	const lastModifiedDate = '04/16/2023';
+	const lastModifiedDate = '04/25/2023';
 	return (
 		<>
 			<Header underConstruction={underConstruction} />
