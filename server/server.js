32b703786dd07db1c255618ae397cd7b9a397d06
Audrey Gillies <audrey.gillies@gmail.com>
2023-07-01 16:12:55 -0600

    confirming environment in droplet

--- server/server.js
+++ server/server.js
@@ -31,5 +31,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
 	app.listen(PORT, () => {
 		console.log(`API server running on port ${PORT}!`);
+		console.log(process.env.NODE_ENV);
 	});
 });
