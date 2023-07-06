b516b8ffec588159a4965be5d5dc8e9c374d6bc2
Audrey Gillies <audrey.gillies@gmail.com>
2023-07-06 15:02:35 -0600

    updating the login form to use formik

--- client/src/views/pages/auth/Login.js
+++ client/src/views/pages/auth/Login.js
@@ -1,105 +1,107 @@
-import { useState } from 'react';
-import { Grid, useTheme, useMediaQuery } from '@mui/material';
+import { Grid, useTheme } from '@mui/material';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 // project imports
-import FormInput from 'views/components/forms/inputs/index';
+import FormInput from 'views/components/forms/inputs';
 import SubmitButton from 'views/components/SubmitButton';
 import useAuth from 'hooks/useAuth';
 
-const Login = () => {
+const Login = ({ onSubmit }) => {
 	const theme = useTheme();
-	const lgAndUp = useMediaQuery(theme.breakpoints.up('md'));
 
 	const { getAuthToken } = useAuth();
 
-	const initialState = { email: '', password: '' };
-	const [formState, setFormState] = useState(initialState);
-
-	// update state based on form input changes
-	const handleChange = (event) => {
-		const { name, value } = event.target;
-
-		setFormState({
-			...formState,
-			[name]: value
-		});
-		setSubmissionDisabled({
-			...submissionDisabled,
-			disabled: !formState.email || !formState.password
-		});
-	};
-
-	// submit form
-	const handleFormSubmit = async (event) => {
-		event.preventDefault();
-		try {
-			await getAuthToken({ ...formState });
-		} catch (err) {
-			console.error(err);
-		}
-		setFormState(initialState);
-	};
-
-	const submissionDisabledInitialState = {
-		disabled: true,
-		message: 'Enter your email and password.'
-	};
-
-	const [submissionDisabled, setSubmissionDisabled] = useState(submissionDisabledInitialState);
+	const blankInfo = { email: '', password: '' };
 
 	return (
-		<>
-			<form>
-				<Grid container spacing={theme.spacing(lgAndUp ? 3 : 2)}>
-					<Grid item xs={12}>
-						<FormInput
-							componentType={'text'}
-							componentProps={{
-								placeholder: '',
-								name: 'email',
-								type: 'email',
-								value: formState.email,
-								onChange: handleChange
-							}}
-							label={'Email'}
-							required={true}
-							// error={!formState.username}
-							helperText={'Email is required.'}
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<FormInput
-							componentType={'text'}
-							componentProps={{
-								placeholder: '',
-								name: 'password',
-								type: 'password',
-								value: formState.password,
-								onChange: handleChange
-							}}
-							label={'Password'}
-							required={true}
-							// error={!formState.password}
-							helperText={'Password is required.'}
-						/>
-					</Grid>
-					<Grid container spacing={theme.spacing()} sx={{ justifyContent: 'flex-end' }}>
-						<Grid item>
-							<SubmitButton
-								title={'Login'}
-								tooltipText={
-									!submissionDisabled.disabled
-										? 'Login'
-										: submissionDisabled.message
-								}
-								onClick={handleFormSubmit}
-								disabled={submissionDisabled.disabled}
-							/>
+		<Formik
+			initialValues={blankInfo}
+			enableReinitialize
+			validationSchema={Yup.object().shape({
+				email: Yup.string()
+					.email('Must be a valid email.')
+					.trim()
+					.required('Email is required.'),
+				password: Yup.string().required('Password is required.')
+			})}
+			onSubmit={async (values, { setStatus, setSubmitting }) => {
+				try {
+					await getAuthToken(values);
+					onSubmit && onSubmit();
+				} catch (err) {
+					console.error(err);
+				}
+			}}
+		>
+			{({
+				handleChange,
+				handleBlur,
+				handleSubmit,
+				isSubmitting,
+				touched,
+				errors,
+				values
+			}) => {
+				return (
+					<form noValidate>
+						<Grid container spacing={theme.spacing()}>
+							<Grid item xs={12}>
+								<FormInput
+									componentType={'text'}
+									componentProps={{
+										name: 'email',
+										type: 'email',
+										value: values.email,
+										onChange: handleChange,
+										onBlur: handleBlur
+									}}
+									label={'Email'}
+									required={true}
+									error={Boolean(touched.email && errors.email)}
+									helperText={touched.email && errors.email && `${errors.email}`}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<FormInput
+									componentType={'text'}
+									componentProps={{
+										name: 'password',
+										type: 'password',
+										value: values.password,
+										onChange: handleChange,
+										onBlur: handleBlur
+									}}
+									label={'Password'}
+									required={true}
+									error={Boolean(touched.password && errors.password)}
+									helperText={
+										touched.password && errors.password && `${errors.password}`
+									}
+								/>
+							</Grid>
+							<Grid item xs={12} sx={{ margin: '1rem 0 0' }}>
+								<Grid
+									container
+									sx={{ justifyContent: 'flex-end', alignItems: 'center' }}
+									spacing={theme.spacing()}
+								>
+									<Grid item>
+										<SubmitButton
+											disableElevation
+											disabled={isSubmitting}
+											title={'Login'}
+											onClick={handleSubmit}
+											placeholder={'Login'}
+										/>
+									</Grid>
+								</Grid>
+							</Grid>
 						</Grid>
-					</Grid>
-				</Grid>
-			</form>
-		</>
+					</form>
+				);
+			}}
+		</Formik>
 	);
 };
 
