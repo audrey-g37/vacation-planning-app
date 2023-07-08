81fe33b283dae0a475df4887f3e47efc7b6a405d
Audrey Gillies <audrey.gillies@gmail.com>
2023-07-08 13:38:09 -0600

    adding a view and edit icon to the grid view and setting the ability to allow selction of rows

--- client/src/views/components/data-grid/index.js
+++ client/src/views/components/data-grid/index.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { Box, useTheme } from '@mui/material';
 import { DataGrid as DataGridDisplay } from '@mui/x-data-grid';
+import ListAltIcon from '@mui/icons-material/ListAlt';
 import EditIcon from '@mui/icons-material/Edit';
 
 // project imports
 import Form from '../forms';
-import CustomTypography from '../CustomTypography';
 import SubmitButton from '../SubmitButton';
 import { formatValue } from './formatting';
+import useAuth from 'hooks/useAuth';
 
 const DataGrid = ({
 	rows,
@@ -17,10 +18,12 @@ const DataGrid = ({
 	pageSizeOptions = [10, 25, 50],
 	collection,
 	queryResults,
+	allowSelection = false,
 	hidePagination = false,
 	maxHeight = '70vh'
 }) => {
 	const theme = useTheme();
+	const { navigate } = useAuth();
 	const [dialogOpen, setDialogOpen] = useState({ open: false, formData: {} });
 
 	const headerStylingObj = {
@@ -30,7 +33,51 @@ const DataGrid = ({
 		color: theme.palette.text.secondary
 	};
 
-	rows = rows.map((row) => {
+	if (viewIcon) {
+		columns.push({
+			field: 'view',
+			headerName: 'View',
+			width: 100,
+			editable: false,
+			renderCell: ({ row }) => (
+				<SubmitButton
+					icon={<ListAltIcon />}
+					tooltipText={'View Details'}
+					onClick={async () => {
+						navigate(`/view-${collection}/${row._id}`);
+					}}
+					customStyle={{
+						color: theme.palette.text.primary
+					}}
+				/>
+			)
+		});
+	}
+	if (editIcon) {
+		columns.push({
+			field: 'edit',
+			headerName: 'Edit',
+			width: 100,
+			editable: false,
+			renderCell: ({ row }) => (
+				<SubmitButton
+					icon={<EditIcon />}
+					tooltipText={'Edit'}
+					onClick={async () => {
+						setDialogOpen({
+							...dialogOpen,
+							open: true,
+							formData: rows.find((row) => row._id === row._id)
+						});
+					}}
+					customStyle={{
+						color: theme.palette.text.primary
+					}}
+				/>
+			)
+		});
+	}
+	const displayRows = rows.map((row) => {
 		for (let [key, value] of Object.entries(row)) {
 			const colMatch = columns.find((col) => col.field === key);
 			if (colMatch?.format || colMatch?.type === 'date') {
@@ -49,14 +96,24 @@ const DataGrid = ({
 	return (
 		<Box sx={{ maxHeight: maxHeight, width: '100%' }}>
 			<DataGridDisplay
-				rows={rows}
+				rows={displayRows}
 				columns={columns}
 				pageSizeOptions={pageSizeOptions}
-				checkboxSelection
+				checkboxSelection={allowSelection}
 				disableRowSelectionOnClick
 				hideFooter={hidePagination}
 				sx={{ '& .MuiDataGrid-columnHeaders': headerStylingObj }}
 			/>
+			{dialogOpen && (
+				<Form
+					isOpen={dialogOpen.open}
+					formData={dialogOpen.formData}
+					setClosed={() => setDialogOpen({ ...dialogOpen, open: false })}
+					queryResults={queryResults}
+					collection={collection}
+					edit={true}
+				/>
+			)}
 		</Box>
 	);
 };
