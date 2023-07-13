4f3d9e815dbfc8cd0d3197d1ea30c3f04bbdda4e
Audrey Gillies <audrey.gillies@gmail.com>
2023-07-13 13:54:47 -0600

    adding the dialog for the user to confirm before the request a friend; checking if a request has already been created

--- client/src/views/components/forms/FriendRequest.js
+++ client/src/views/components/forms/FriendRequest.js
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import { Grid, useTheme } from '@mui/material';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 // project imports
 import FormInput from './inputs';
+import ConfirmationDialog from '../ConfirmationDialog';
 import SubmitButton from '../SubmitButton';
 import useAuth from 'hooks/useAuth';
 
 const FriendRequestForm = ({ onSubmit }) => {
 	const { user, crudFunctions } = useAuth();
 
-	const { getUser, addFriendRequest } = crudFunctions;
+	const { getUser, addFriendRequest, getFriendRequests } = crudFunctions;
 
 	const theme = useTheme();
 
@@ -19,6 +21,50 @@ const FriendRequestForm = ({ onSubmit }) => {
 		requestedByUserID: user._id,
 		pendingApprovalUserID: '',
 		pendingApprovalUserEmail: ''
+	};
+
+	const [confirmation, setConfirmation] = useState({
+		open: false,
+		data: blankInfo,
+		newUser: false
+	});
+
+	const confirmAddFriend = async () => {
+		await addFriendRequest(confirmation.data);
+		setConfirmation({
+			...confirmation,
+			open: false,
+			data: blankInfo
+		});
+		if (confirmation.newUser) {
+			// todo email the user and invite them to join
+		}
+		onSubmit && (await onSubmit());
+	};
+
+	const checkForExistingRequestMatches = async () => {
+		// checking for requests that the user has already made to this person
+		const ourDataToSend = {
+			variables: {
+				requestedByUserID: user._id,
+				pendingApprovalUserID: confirmation.pendingApprovalUserID
+			}
+		};
+		// used to check for requests that exist for user to review (made by who user is requesting)
+		const theirDataToSend = {
+			variables: {
+				requestedByUserID: confirmation.pendingApprovalUserID,
+				pendingApprovalUserID: user._id
+			}
+		};
+		const { data: existingRequest1 } = await getFriendRequests(ourDataToSend);
+		const { data: existingRequest2 } = await getFriendRequests(theirDataToSend);
+		if (
+			existingRequest1?.friendRequests?.length > 0 ||
+			existingRequest2?.friendRequests?.length > 0
+		) {
+			return existingRequest1?.friendRequests?.[0] || existingRequest2?.friendRequests?.[0];
+		}
 	};
 
 	return (
@@ -37,10 +83,11 @@ const FriendRequestForm = ({ onSubmit }) => {
 						variables: values
 					};
 					const { data } = await getUser({
-						variables: { email: dataToSend.variables.pendingApprovalUserEmail }
+						variables: {
+							email: dataToSend.variables.pendingApprovalUserEmail.toLowerCase()
+						}
 					});
 					if (data?.user) {
-						// todo search for an existing request with the user IDs so we can't request multiple times
 						dataToSend = {
 							...dataToSend,
 							variables: {
@@ -48,16 +95,27 @@ const FriendRequestForm = ({ onSubmit }) => {
 								pendingApprovalUserID: data.user._id
 							}
 						};
-						// todo alert the user they are about to request {email address} and confirm they want to continue
-						// on continue
-						await addFriendRequest(dataToSend)
-							.then((res) => console.log({ res }))
-							.catch((err) => console.error({ err }));
+						const existingRequest = await checkForExistingRequestMatches();
+						if (!existingRequest) {
+							setConfirmation({
+								...confirmation,
+								open: true,
+								data: dataToSend,
+								newUser: false
+							});
+						} else {
+							// todo set an alert and show message to user with request status and who request is pending
+							console.log('A request has already been created', { existingRequest });
+							onSubmit && onSubmit();
+						}
 					} else {
-						// todo alert user and ask them if they want to invite the user to join GRIP or try searching another email.
-						console.log('No user with matching email.');
+						setConfirmation({
+							...confirmation,
+							open: true,
+							data: dataToSend,
+							newUser: true
+						});
 					}
-					onSubmit && onSubmit();
 				} catch (err) {
 					console.error(err);
 				}
@@ -75,50 +133,71 @@ const FriendRequestForm = ({ onSubmit }) => {
 				values
 			}) => {
 				return (
-					<form noValidate>
-						<Grid container spacing={theme.spacing()}>
-							<Grid item xs={12}>
-								<FormInput
-									componentType={'text'}
-									componentProps={{
-										name: 'pendingApprovalUserEmail',
-										type: 'email',
-										value: values.pendingApprovalUserEmail,
-										onChange: handleChange,
-										onBlur: handleBlur
-									}}
-									label={'Email'}
-									required={true}
-									error={Boolean(
-										touched.pendingApprovalUserEmail &&
-											errors.pendingApprovalUserEmail
-									)}
-									helperText={
-										touched.pendingApprovalUserEmail &&
-										errors.pendingApprovalUserEmail &&
-										`${errors.pendingApprovalUserEmail}`
-									}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<Grid
-									container
-									sx={{ justifyContent: 'flex-end', alignItems: 'center' }}
-									spacing={theme.spacing()}
-								>
-									<Grid item>
-										<SubmitButton
-											disableElevation
-											disabled={isSubmitting}
-											title={'Save Changes'}
-											onClick={handleSubmit}
-											placeholder={'Request'}
-										/>
+					<>
+						<form noValidate>
+							<Grid container spacing={theme.spacing()}>
+								<Grid item xs={12}>
+									<FormInput
+										componentType={'text'}
+										componentProps={{
+											name: 'pendingApprovalUserEmail',
+											type: 'email',
+											value: values.pendingApprovalUserEmail,
+											onChange: handleChange,
+											onBlur: handleBlur
+										}}
+										label={'Email'}
+										required={true}
+										error={Boolean(
+											touched.pendingApprovalUserEmail &&
+												errors.pendingApprovalUserEmail
+										)}
+										helperText={
+											touched.pendingApprovalUserEmail &&
+											errors.pendingApprovalUserEmail &&
+											`${errors.pendingApprovalUserEmail}`
+										}
+									/>
+								</Grid>
+								<Grid item xs={12}>
+									<Grid
+										container
+										sx={{ justifyContent: 'flex-end', alignItems: 'center' }}
+										spacing={theme.spacing()}
+									>
+										<Grid item>
+											<SubmitButton
+												disableElevation
+												disabled={isSubmitting}
+												title={'Add Friend'}
+												onClick={handleSubmit}
+												placeholder={'Request'}
+											/>
+										</Grid>
 									</Grid>
 								</Grid>
 							</Grid>
-						</Grid>
-					</form>
+						</form>
+						{confirmation.open && (
+							<ConfirmationDialog
+								open={confirmation.open}
+								setClosed={() => setConfirmation({ ...confirmation, open: false })}
+								onCancel={() =>
+									setConfirmation({
+										...confirmation,
+										open: false,
+										data: blankInfo
+									})
+								}
+								onConfirm={confirmAddFriend}
+								textContent={
+									confirmation.newUser
+										? `There is no user with the email ${values.pendingApprovalUserEmail}.  Send an email inviting them to create a GRIP account?`
+										: `You are about to send a friend request to ${values.pendingApprovalUserEmail}.`
+								}
+							/>
+						)}
+					</>
 				);
 			}}
 		</Formik>
