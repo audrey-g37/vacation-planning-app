ba669287cb706febd40bd04b78949bbb7342e967
Audrey Gillies <audrey.gillies@gmail.com>
2023-07-23 14:08:37 -0600

    updating single trip view to show the attendees

--- client/src/views/pages/ViewTrip/ViewSingleTrip.js
+++ client/src/views/pages/ViewTrip/ViewSingleTrip.js
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router';
-import { Grid } from '@mui/material';
+import { Grid, useTheme } from '@mui/material';
 
 // project imports
 import MainCard from 'views/components/MainCard';
@@ -7,37 +7,74 @@ import useAuth from 'hooks/useAuth';
 import { useEffect, useState } from 'react';
 import TripDetails from './TripDetails';
 import CircularLoader from 'views/components/CircularLoader';
+import ViewAttendeesForTrip from './TripAttendeeGrid';
 
-const ViewSingleTrip = ({ data }) => {
+const ViewSingleTrip = () => {
+	const theme = useTheme();
 	const { id } = useParams();
 	const { crudFunctions } = useAuth();
-	const { getSingleTrip } = crudFunctions;
+	const { getSingleTrip, getTripAttendeesByTripID } = crudFunctions;
 
-	const [tripData, setTripData] = useState(data || {});
+	const [tripData, setTripData] = useState({ tripDetails: {}, tripAttendees: [] });
 	const [loading, setLoading] = useState(false);
 
 	const getTripData = async () => {
-		setLoading(true);
 		const { data } = await getSingleTrip({ variables: { queryID: id } });
-		setTripData(data.trip);
+		return data.trip;
+	};
+
+	const getAttendeeData = async () => {
+		const { data } = await getTripAttendeesByTripID({
+			variables: { tripID: id }
+		});
+		return data.tripAttendeesByTripID;
+	};
+
+	const setAllTripData = async (trip = true, attendees = true) => {
+		setLoading(true);
+		let tripDataObj = {};
+		if (trip) {
+			const tripDetails = await getTripData();
+			tripDataObj = { ...tripDataObj, tripDetails: tripDetails };
+		}
+		if (attendees) {
+			const attendees = await getAttendeeData();
+			tripDataObj = { ...tripDataObj, tripAttendees: attendees };
+		}
+		setTripData(tripDataObj);
 		setLoading(false);
 	};
 
 	useEffect(() => {
-		!data && getTripData();
+		setAllTripData();
 	}, []);
 
 	return (
-		<MainCard
-			title={tripData.title}
-			editItem={'trip'}
-			formData={tripData}
-			collection={'trip'}
-			queryResults={getTripData}
-		>
+		<Grid container spacing={theme.spacing()}>
 			{loading && <CircularLoader />}
-			<TripDetails data={tripData} />
-		</MainCard>
+			<Grid item xs={12}>
+				<MainCard
+					title={tripData.tripDetails?.title}
+					editItem={'trip'}
+					formData={tripData.tripDetails}
+					collection={'trip'}
+					queryResults={() => setAllTripData(true, false)}
+				>
+					<TripDetails data={tripData.tripDetails} />
+				</MainCard>
+			</Grid>
+			<Grid item xs={12} md={4}>
+				<MainCard
+					title={'Attendees'}
+					collection={'tripAttendee'}
+					newItem='Trip Attendee'
+					queryResults={() => setAllTripData(false, true)}
+					sx={{ margin: '0 1rem' }}
+				>
+					{!loading && <ViewAttendeesForTrip data={tripData.tripAttendees} />}
+				</MainCard>
+			</Grid>
+		</Grid>
 	);
 };
 
