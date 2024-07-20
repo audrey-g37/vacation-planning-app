38d4946bcd09fd1f6aa31d289baf56e9518a3a13
Audrey Gillies <audrey.gillies@gmail.com>
2024-07-20 07:16:53 -0600

    removing old files for tasks and using budget file as start for task form

---
+++ client/src/views/components/forms/Task.js
@@ -0,0 +1,245 @@
+import { useState, useEffect } from 'react';
+import { Grid, useTheme } from '@mui/material';
+import { useParams } from 'react-router';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+// project imports
+import FormInput from './inputs';
+import SubmitButton from '../SubmitButton';
+import useAuth from 'hooks/useAuth';
+import CustomDivider from '../CustomDivider';
+
+const TaskForm = ({ edit, formData, onSubmit }) => {
+	const theme = useTheme();
+
+	const { crudFunctions, alert, setAlert } = useAuth();
+
+	const { addTask, editTask } = crudFunctions;
+
+	const { id: tripID } = useParams();
+
+	const [dropdownOfAttendees, setDropdownOfAttendees] = useState([]);
+
+	// todo: update with task fields
+	const blankInfo = {
+		title: '',
+		maxAmount: 0,
+		actualAmount: 0,
+		purchaseDate: '',
+		purchasedByUserID: null,
+		splitByUserIDs: [],
+		tripID: tripID,
+		taskID: null
+	};
+
+	const createDropdownOfAttendees = () => {
+		const attendees = formData.tripAttendees.map((attendee) => {
+			return {
+				value: attendee.attendeeUserID._id,
+				label: `${attendee.attendeeUserID.firstName} ${attendee.attendeeUserID.lastName}`
+			};
+		});
+		setDropdownOfAttendees(attendees);
+	};
+
+	useEffect(() => {
+		createDropdownOfAttendees();
+	}, []);
+
+	return (
+		<Formik
+			initialValues={edit ? formData : blankInfo}
+			enableReinitialize
+			// todo: update with task fields
+			validationSchema={Yup.object().shape({
+				title: Yup.string().trim().required('Title is required.'),
+				maxAmount: Yup.number().test({
+					name: 'hundredths-place',
+					test: (num) =>
+						(String(num).includes('.')
+							? String(num).split('.')[1]?.length <= 2
+							: String(num)) && num >= 0,
+					message: 'Must be a valid dollar amount.'
+				}),
+				actualAmount: Yup.number().test({
+					name: 'hundredths-place',
+					test: (num) =>
+						(String(num).includes('.')
+							? String(num).split('.')[1]?.length <= 2
+							: String(num)) && num >= 0,
+					message: 'Must be a valid dollar amount.'
+				})
+			})}
+			onSubmit={async (values, { setStatus, setSubmitting }) => {
+				try {
+					let dataToSend = {
+						variables: values
+					};
+					if (edit) {
+						await editTask(dataToSend);
+					} else {
+						await addTask(dataToSend);
+					}
+					setAlert({
+						...alert,
+						open: true,
+						severity: 'success',
+						message: `Task successfully saved!`
+					});
+					onSubmit && (await onSubmit());
+				} catch (err) {
+					setAlert({
+						...alert,
+						open: true,
+						severity: 'error',
+						message: `There was a problem trying to save the task.  Please try again later.`
+					});
+				}
+			}}
+		>
+			{({
+				handleChange,
+				handleBlur,
+				setTouched,
+				handleSubmit,
+				setFieldValue,
+				isSubmitting,
+				touched,
+				errors,
+				values
+			}) => {
+				// todo: update with task fields
+				return (
+					<form noValidate>
+						<Grid container spacing={theme.spacing()}>
+							<Grid item xs={12} md={6}>
+								<FormInput
+									componentType={'text'}
+									componentProps={{
+										name: 'title',
+										onChange: handleChange,
+										onBlur: handleBlur,
+										value: values.title
+									}}
+									label={'Title'}
+									required={true}
+									error={Boolean(touched.title && errors.title)}
+									helperText={touched.title && errors.title && `${errors.title}`}
+								/>
+							</Grid>
+							<Grid item xs={12} md={6}>
+								<FormInput
+									componentType={'text'}
+									componentProps={{
+										name: 'maxAmount',
+										onChange: handleChange,
+										onBlur: handleBlur,
+										type: 'number',
+										value: values.maxAmount
+									}}
+									label={'Maximum Amount'}
+									error={Boolean(touched.maxAmount && errors.maxAmount)}
+									helperText={
+										touched.maxAmount &&
+										errors.maxAmount &&
+										`${errors.maxAmount}`
+									}
+								/>
+							</Grid>
+							<Grid item xs={12} md={6}>
+								<FormInput
+									componentType={'text'}
+									componentProps={{
+										name: 'actualAmount',
+										onChange: handleChange,
+										onBlur: handleBlur,
+										type: 'number',
+										value: values.actualAmount
+									}}
+									label={'Amount Spent'}
+									error={Boolean(touched.actualAmount && errors.actualAmount)}
+									helperText={
+										touched.actualAmount &&
+										errors.actualAmount &&
+										`${errors.actualAmount}`
+									}
+								/>
+							</Grid>
+							<Grid item xs={12} md={6}>
+								<FormInput
+									componentType={'dateTime'}
+									componentProps={{
+										name: 'purchaseDate',
+										value: values.purchaseDate,
+										multiline: true,
+										minRows: 3,
+										onChange: setFieldValue,
+										onBlur: setTouched
+									}}
+									label={'Purchase Date'}
+								/>
+							</Grid>
+						</Grid>
+						<CustomDivider margin='0.5rem' />
+						<Grid container spacing={theme.spacing()}>
+							<Grid item xs={12} md={6}>
+								<FormInput
+									componentType={'autocomplete'}
+									componentProps={{
+										name: 'purchasedByUserID',
+										options: dropdownOfAttendees,
+										multiple: false,
+										onChange: setFieldValue,
+										onBlur: handleBlur,
+										value: values.purchasedByUserID
+									}}
+									label={'Purchased By'}
+								/>
+							</Grid>
+							<Grid item xs={12} md={6}>
+								<FormInput
+									componentType={'autocomplete'}
+									componentProps={{
+										name: 'splitByUserIDs',
+										options: dropdownOfAttendees,
+										multiple: true,
+										onChange: setFieldValue,
+										onBlur: handleBlur,
+										value: values.splitByUserIDs
+									}}
+									label={'Splitting Cost'}
+									helperText={
+										'Who will be splitting the cost for this purchase. Used to calculate individual spending.'
+									}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<Grid
+									container
+									sx={{
+										justifyContent: 'flex-end',
+										alignItems: 'center'
+									}}
+									spacing={theme.spacing()}
+								>
+									<Grid item>
+										<SubmitButton
+											disableElevation
+											disabled={isSubmitting}
+											title={`Save ${edit ? ' Changes' : ''}`}
+											onClick={handleSubmit}
+											placeholder={'Save Changes'}
+										/>
+									</Grid>
+								</Grid>
+							</Grid>
+						</Grid>
+					</form>
+				);
+			}}
+		</Formik>
+	);
+};
+
+export default TaskForm;
--- client/src/views/pages/index.js
+++ client/src/views/pages/index.js
@@ -2,18 +2,7 @@ import Signup from './auth/Register';
 import Login from './auth/Login';
 import UnderConstruction from './ErrorPages/UnderConstruction';
 import Dashboard from './Dashboard/Dashboard';
-import ViewTask from './ViewTask/ViewTask';
 import ViewSingleTrip from './ViewTrip/ViewSingleTrip';
 import ViewAllTrips from './ViewTrip/ViewAllTrips';
-import EditTask from '../components/forms/EditTask';
 
-export {
-	Signup,
-	Login,
-	UnderConstruction,
-	Dashboard,
-	ViewTask,
-	ViewSingleTrip,
-	ViewAllTrips,
-	EditTask
-};
+export { Signup, Login, UnderConstruction, Dashboard, ViewSingleTrip, ViewAllTrips };
